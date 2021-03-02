9f5ef6ceccc6539df5488c6a12e4cac7f09c0133
Panayot Kulchev <panayot.kulchev@taryafintech.com>
2021-03-02 15:38:57 +0200

    allow to edit server

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 
 import {ServersService} from '../servers.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {relative} from '@angular/compiler-cli/src/ngtsc/file_system';
 
 @Component({
   selector: 'app-server',
@@ -12,7 +13,8 @@ export class ServerComponent implements OnInit {
   server: { id: number, name: string, status: string };
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -21,7 +23,12 @@ export class ServerComponent implements OnInit {
     this.route.params.subscribe(
       (params: Params) => {
         this.server = this.serversService.getServer(+params['id']);
+        console.log('Params subscribe: ' + this.server.id);
       }
     );
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 }
