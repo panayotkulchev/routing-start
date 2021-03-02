36f36c169c60bc6b210a2ff937ffd954b2f09f55
Panayot Kulchev <panayot.kulchev@taryafintech.com>
2021-03-02 16:13:06 +0200

    extract app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,22 +11,8 @@ import {UserComponent} from './users/user/user.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
-import {RouterModule, Routes} from '@angular/router';
-
-const appRouts: Routes = [
-  {path: '', component: HomeComponent},
-  {
-    path: 'users', component: UsersComponent, children: [
-      {path: ':id/:name', component: UserComponent}
-    ]
-  },
-  {
-    path: 'servers', component: ServersComponent, children: [
-      {path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent},
-    ]
-  }
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -36,12 +22,13 @@ const appRouts: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRouts)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
